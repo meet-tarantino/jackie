874f109175c1f72d850af8bd8545481d78bc159c
Darren McElligott <darren.mcelligott@gmail.com>
2018-07-17 11:15:04 -0400

    oops.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const mongo = require('mongodb');
 const db = mongo.connect(config.mongo.url, { promiseLibrary: Promise });
 
 const server = createServer(config, log, db);
-console.log('server', server);
 server.listen(config.port, () => {
   log.info(`${config.name} server started: listening on port ${config.port}`);
 });
