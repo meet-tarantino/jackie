39d983974673f2852d3d2a258a725d7232b59f52
Darren McElligott <darren.mcelligott@gmail.com>
2018-07-17 11:11:49 -0400

    add dockerfile

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const Promise = require('bluebird');
 const mongo = require('mongodb');
 const db = mongo.connect(config.mongo.url, { promiseLibrary: Promise });
 
-createServer(config, log, db)
-  .listen(config.port, () => {
-    log.info(`${config.name} server started: listening on port ${config.port}`);
-  });
+const server = createServer(config, log, db);
+console.log('server', server);
+server.listen(config.port, () => {
+  log.info(`${config.name} server started: listening on port ${config.port}`);
+});
